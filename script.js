e230aa3d105dc7bcf68e75f99bebcc8351ac6936
Ve <22580445+Veo1oeV@users.noreply.github.com>
2018-09-30 12:00:26 -0400

    Seconds field is no longer used.

--- script.js
+++ script.js
@@ -19,10 +19,8 @@ function displayDate() {
 	// Time display section
 	h = now.getHours(),
 	m = now.getMinutes(),
-	s = now.getSeconds();
 	if (h < 10) h = '0' + h;
 	if (m < 10) m = '0' + m;
-	if (s < 10) s = '0' + s;
 	document.getElementById('time').innerHTML = h + ':' + m;
 }
 
